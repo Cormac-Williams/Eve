f559ace5453a525e5839b05466c2e0f10607ddf1
Cormac Williams <Cormac.Williams@Gmail.com>
2016-11-30 23:13:50 +0000

    Remove infinity check. Handle Div by zero. Revert getProposal

--- src/runtime/providers/math.ts
+++ src/runtime/providers/math.ts
@@ -14,7 +14,7 @@ abstract class TotalFunctionConstraint extends Constraint {
   resolveProposal(proposal, prefix) {
     let {args} = this.resolve(prefix);
     let result = this.getReturnValue(args);
-    if (isNaN(result) || !(isFinite(result))) {return [];}
+    if (isNaN(result)) {return [];}
     return [result];
   }
 
@@ -26,13 +26,10 @@ abstract class TotalFunctionConstraint extends Constraint {
 
   // Total functions always have a cardinality of 1
   getProposal(tripleIndex, proposed, prefix) {
-    if(this.returns.length) {
-      let proposal = this.proposalObject;
-      proposal.providing = proposed;
-      proposal.cardinality = 1;
-      return proposal;
-    }
-    return;
+    let proposal = this.proposalObject;
+    proposal.providing = proposed;
+    proposal.cardinality = 1;
+    return proposal;
   }
 }
 
